8fb4a7f72797d5559a9e3db5c08690f119beb096
Karunakar Bhogyari <Karunakar.Bhogyari@brandmuscle.com>
2018-08-29 12:04:07 +0530

    ajax call with jquery get method

--- async.js
+++ async.js
@@ -1,17 +1,25 @@
 window.onload = function () {
 
-    var http = new XMLHttpRequest();
+    // var http = new XMLHttpRequest();
 
-    http.onreadystatechange = function () {
-        console.log(http);
-        if (http.readyState == 4 && http.status == 200) {
-            console.log(JSON.parse(http.response));
-        }
-    };
+    // http.onreadystatechange = function () {
+    //     console.log(http);
+    //     if (http.readyState == 4 && http.status == 200) {
+    //         console.log(JSON.parse(http.response));
+    //     }
+    // };
 
-    http.open("GET", "data/tweets.json", false);
-    http.send();
-    console.log("Testdddddddd");
+    // http.open("GET", "data/tweets.json", false);
+    // http.send();
+    // console.log("Testdddddddd");
+
+    //Same with Jquery
+
+    $.get("data/tweets.json", function (d) {
+        console.log(d);
+    });
+
+    console.log("It will be called before get method response");
 };
 
 /*
